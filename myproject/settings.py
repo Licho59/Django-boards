137a68a7712e90dc4a005eeb06702ddd97eb84c2
Licho59 <lestlalka@gmail.com>
2018-06-03 17:05:37 +0200

    "Upgrade to initial commit." :wq

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+from decouple import config, Csv
+import dj_database_url
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,12 +24,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3&*5yg&l%^)0-)+@nu!xeqnl2zpz*h9df&5gx&rduqb+-a*=2a'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+DATABASES = {
+    'default': dj_database_url.config(default=config('DATABASE_URL')
+    )
+}
 
 
 # Application definition
